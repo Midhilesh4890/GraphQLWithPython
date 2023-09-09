a42dcb64c3bb218b72d63fa551f670f62ae9ea48
midhilesh <chintu.midhilesh@gmail.com>
2023-09-09 22:42:38 +0530

    Adding basic code

---
+++ main.py
@@ -0,0 +1,25 @@
+from graphene import Schema, ObjectType, String
+
+class Query(ObjectType):
+
+    query = String(name = String(default_value = '<Need to insert a Name>'))
+    hello = String(name = String(default_value = 'World'))
+
+    def resolve_query(self, info, name):
+        return f'Hello {name}'
+    
+    def resolve_hello(self, info, name):
+        return f'Hello {name}'
+
+schema = Schema(query= Query)
+
+gql = '''
+        {
+            query(name:"Midhilesh"),
+            hello
+        }
+      '''
+
+if __name__ == '__main__':
+    result = schema.execute(gql)
+    print(result)
